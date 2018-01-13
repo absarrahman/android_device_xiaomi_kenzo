30b93b9380d845422341624373fcecb89a07d6c5
Shamith <33453856+shamith16@users.noreply.github.com>
2018-01-13 11:50:41 +0000

    Update and rename cardinal.mk to aosp_.mk

--- aosp_.mk
+++ aosp_.mk
@@ -18,14 +18,18 @@
 
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
-# Inherit some common Cardinal-AOSP stuff.
-$(call inherit-product, vendor/cardinal/common.mk)
+# Inherit some common AOSP stuff.
+TARGET_ARCH := arm64
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_ARCORE := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := cardinal_kenzo
+PRODUCT_NAME := aosp_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
