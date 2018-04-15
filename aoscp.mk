bfe1509856fc71ad70e468fdea5ebd6d2e29785e
absarrahman <raman040635@gmail.com>
2018-04-15 13:40:13 +0000

    kenzo: Update build FP
    Some roms eg AOSCP has that pop out Your vendor image does not match the system. Please flash MMB29M vendor image for your device. This is the fix for it. This commit fixes that pop out. Signed-off-by: Absar <raman040635@gmail.com>

--- aoscp.mk
+++ aoscp.mk
@@ -39,6 +39,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V9.2.4.0.MHOMIEK:user/release-keys \
+
     PRIVATE_BUILD_DESC="kenzo-user 6.0.1 V9.2.4.0.MHOMIEK release-keys"
+
+BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V9.2.4.0.MHOMIEK:user/release-keys
 endif
