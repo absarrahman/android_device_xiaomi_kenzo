331ec5a92e708060ce77b7bfb852eaa785b18c81
Adarsh-MR <adarshmr1998@gmail.com>
2018-12-15 20:20:19 +0530

    Kenzo: Re-enable sepolicies inclusion
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,8 @@ TARGET_KERNEL_CONFIG := kenzo_defconfig
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Sepolicy
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # inherit from the proprietary version
 -include vendor/xiaomi/kenzo/BoardConfigVendor.mk
